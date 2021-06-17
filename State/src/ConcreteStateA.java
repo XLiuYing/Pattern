eddaab0dd28e78436bc411967ea43c139482b837
Lying <2486819446@qq.com>
2021-06-17 21:00:03 +0800

    first commit

---
+++ State/src/ConcreteStateA.java
@@ -0,0 +1,6 @@
+public class ConcreteStateA extends State {
+    public void Handle(Context context) {
+        System.out.println("当前状态是 A.");
+        context.setState(new ConcreteStateA());
+    }
+}
